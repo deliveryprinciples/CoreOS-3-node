a079be979951a5b7fd1dcc9041048e25641a98c3
Andy Hughes <deliveryprinciples@gmail.com>
2016-11-04 18:04:44 +0000

    working up.sh

--- up.sh
+++ up.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 
-export DO_SSH_KEY_FINGERPRINT="7e:0c:0f:08:04:d4:4d:c2:ec:c5:90:d4:8b:d8:f3:db"
-export TOKEN="DOToken"
+export SSH_KEY_ID="7e:0c:0f:08:04:d4:4d:c2:ec:c5:90:d4:8b:d8:f3:db"
+export REGION="lon1"
+export SIZE="512mb"
 
-# A basic Droplet create request.
-curl -X POST "https://api.digitalocean.com/v2/droplets" \
-  -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/json" \
-  -d'{"name":["coreos-01","coreos-02","coreos-03"]","region":"lon1","size":"512mb","private_networking":true,"image":"coreos-stable","user_data":"'"$(cat cloud-config.yml)"'", "ssh_keys":[ "'$DO_SSH_KEY_FINGERPRINT'" ]}'
+curl --request POST "https://api.digitalocean.com/v2/droplets" \
+     --header "Content-Type: application/json" \
+     --header "Authorization: Bearer "$(cat ../DOToken) \
+     --data '{"region":"'"$REGION"'",
+        "image":"coreos-stable",
+        "size":"'"$SIZE"'",
+        "private_networking":true,
+        "ssh_keys":["'"$SSH_KEY_ID"'"],
+        "names": ["etcd-1", "etcd-2", "etcd-3"],
+        "user_data":"'"$(cat cloud-config.yaml)"'"
+      }'
